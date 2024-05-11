4e268641c1e61b2c425b99bfb0b7478b860ceea7
tangzhiyao <335406905@qq.com>
2024-05-11 18:03:31 +0800

    feat: 新增拉勾平台

---
+++ src/plantforms/lagou/firstOpen.js
@@ -0,0 +1,28 @@
+
+
+import { setupSortJobItem,renderSortJobItem } from "../../commonRender";
+import { createDOM } from './index';
+import { mutationContainer, getListValue } from './index'
+// 首次打开页面时是服务端渲染，没法监听接口，但是 html 中保存了列表数据
+export default function firstOpen(data) {
+    mutationContainer().then(dom => {
+        setupSortJobItem(dom);
+        const children = dom?.children;
+        const list = getListValue(data?.props?.pageProps?.initData) || [];
+        if(!children || !list || list.lenth === 0) return;
+            list.forEach((item, index) => {
+                const {
+                    createTime,
+                    companyShortName,
+                }  = item;
+                const dom = children?.[index];
+                if(!dom) return;
+                
+                let tag = createDOM(createTime, companyShortName); 
+                dom.appendChild(tag);
+            });
+            renderSortJobItem(list,(index)=>{
+                return children?.[index];
+            })
+    });
+}
